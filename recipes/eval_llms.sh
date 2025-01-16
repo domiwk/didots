2302733aaa99a206348c6f6dc65ed4f9835320f8
domiwk <dcw19@ic.ac.uk>
2025-01-16 18:19:32 +0000

    Fix path_prefix & comments

--- recipes/eval_llms.sh
+++ recipes/eval_llms.sh
@@ -1,6 +1,5 @@
 OS='Local'
-PATH_PREFIX='/Users/domiceli/Documents/PhD' # change accordingly 
-PROJECT_DIR="${PATH_PREFIX}/DiDOTS"
+PROJECT_DIR="/Users/domiceli/Documents/PhD/DiDOTS"
 python_handle='python'
 DEVICE='mps' # change accordingly (cpu/cuda/mps)
 
@@ -8,36 +7,35 @@ scripts_dir="${PROJECT_DIR}/scripts"
 SEED=666
 
 #LLM_BASES=( Mistral_7B LLama3 Phi3 Gemma2B )
-LLM_BASES=( Mistral_7B )
+LLM_BASES=( Phi3 )
 BASE_MODEL='None'
-#SETTINGS=( 'FS' 'ZS' 'KB' )
+#SETTINGS= '["FS" "ZS" "KB"]'
 SETTINGS='["KB"]'
 
 for LLM_BASE in ${LLM_BASES[@]}; do
     echo LLM: ${LLM_BASE}
 
-    #DATASET_TRAIN_PATH="${PROJECT_DIR}/experiments/${LLM_BASE}/results/MockUp/MockUp_train_obfuscated_${SETTING}_sentences.csv"
-    #DATASET_TEST_PATH="${PROJECT_DIR}/experiments/${LLM_BASE}/results/MockUp/MockUp_test_obfuscated_${SETTING}_sentences.csv"
-    #DATASET_VAL_PATH="${PROJECT_DIR}/experiments/${LLM_BASE}/results/MockUp/MockUp_val_obfuscated_${SETTING}_sentences.csv"
-
-    #### EVAL DiDOTS #####
+    #### EVAL LLMS #####
     EVAL_DATASETS="MockUp" # ADReSS ADReSSo
     SYSTEM="${LLM_BASE}"
-    #SAMPLE='--sample' 
+    
+    # sampling code for LLMS is separate
+    # See main.py in LLM_inference folder
     SAMPLE=''
 
     #whether to train static classifier or not, run only once.
-    #STATIC='--static'
-    STATIC=''
+    STATIC='--static'
+    #STATIC=''
     STATIC_CLASSIFIERS="BERT_sent SVM_sent"
 
-    #ADAPTIVE="--adaptive"
-    ADAPTIVE=''
+    ADAPTIVE="--adaptive"
+    #ADAPTIVE=''
+
     EVAL_ALL_DIR="${PROJECT_DIR}/experiments/evaluations/all"
-    ADA_CLASSIFIERS="SVM_sent"
+    ADA_CLASSIFIERS="BERT_sent SVM_sent"
     
-    #EVAL_DETECTION="--evaluate_detection"
-    EVAL_DETECTION=""
+    EVAL_DETECTION="--evaluate_detection"
+    #EVAL_DETECTION=""
 
     EVAL_QUALITTY="--evaluate_quality"
     #EVAL_QUALITTY=""
@@ -48,6 +46,7 @@ for LLM_BASE in ${LLM_BASES[@]}; do
     SYSTEM_PATH='{"'"${SYSTEM}"'": {"project_dir": "'"${PROJECT_DIR}/experiments/${LLM_BASE}/results"'", "temperatures": '${SETTINGS}', "target_dir": "'"${PROJECT_DIR}/experiments/evaluations/${LLM_BASE}"'", "sampling_script": "None", "ckt": "None", "model_ckp": "None", "obfuscator": "'"${SYSTEM}"'", "compute_const": false, "level": "sent", "by_document": false,"base_model":"'"${BASE_MODEL}"'"}}'
 
     echo "----- STARTING EVALUATION SCRIPT ---"
-    ${python_handle} ${scripts_dir}/main_eval.py --os ${OS} --path_prefix ${PATH_PREFIX} --project_dir ${PROJECT_DIR} --python_handle ${python_handle} --device ${DEVICE} --datasets ${EVAL_DATASETS} --systems "${SYSTEM}" ${STATIC} --static_classifiers ${STATIC_CLASSIFIERS} ${SAMPLE} ${ADAPTIVE} --ada_classifiers ${ADA_CLASSIFIERS} --doc_type doc sent ${EVAL_DETECTION} --classifiers ${EVAL_CLASSIFIERS} ${EVAL_QUALITTY} --quality_metrics ${QUALITY_METRICS} --eval_all_dir ${EVAL_ALL_DIR} --system_paths "${SYSTEM_PATH}"""
+    ${python_handle} ${scripts_dir}/main_eval.py --os ${OS} --path_prefix ${PROJECT_DIR} --project_dir ${PROJECT_DIR} --python_handle ${python_handle} --device ${DEVICE} --datasets ${EVAL_DATASETS} --systems "${SYSTEM}" ${STATIC} --static_classifiers ${STATIC_CLASSIFIERS} ${SAMPLE} ${ADAPTIVE} --ada_classifiers ${ADA_CLASSIFIERS} --doc_type doc sent ${EVAL_DETECTION} --classifiers ${EVAL_CLASSIFIERS} ${EVAL_QUALITTY} --quality_metrics ${QUALITY_METRICS} --eval_all_dir ${EVAL_ALL_DIR} --system_paths "${SYSTEM_PATH}"""
     echo "----- EVALUATION DONE ---"#
+    echo
 done
